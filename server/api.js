f846975529f1f04b210dc12b2167f0f1896fb812
Mason Chen <b08901058@ntu.edu.tw>
2020-12-08 23:57:39 +0800

    user api updated

---
+++ server/api.js
@@ -0,0 +1,122 @@
+const path = require("path"); // NEW
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+
+const DIST_DIR = path.join(__dirname, "../dist"); // NEW
+const HTML_FILE = path.join(DIST_DIR, "index.html"); // NEW
+const courseInfo = require("../course_info/parsed_courses.json");
+const accountInfo = require("../account_info/account_info.json");
+const Constants = require("./constants");
+
+//const { userInfo } = require("os");
+
+const router = express.Router();
+
+//router.use(bodyParser.urlencoded({ extended: false }));
+//router.use(bodyParser.json());
+
+// For mainpage to fetch what courses the user followed
+router.get("/api/user", (req, res) => {
+  const username = req.query.username;
+  const mode = req.query.mode;
+  const following = accountInfo[username]["following"];
+  let followingDetails;
+  const courseList = getCourseTime(following);
+
+  if (mode === "list") {
+    if (following === undefined) {
+      res.status(404).send({ following: [] });
+    } else {
+      followingDetails = response = { following: following };
+      res.status(200).send(courseList);
+    }
+  }
+});
+
+// For fetching course infomations
+router.get("/api/course", (req, res) => {
+  const serial_number = req.query.serial_number.toString();
+  console.log(serial_number);
+  const info = courseInfo[serial_number];
+  console.log(info);
+
+  if (info === undefined) {
+    res.status(404).send({});
+  } else {
+    response = info;
+    res.status(200).send(response);
+  }
+});
+
+// Handle login post
+router.route("/login").post(
+  express.urlencoded({ extended: false }),
+  asyncHandler(async (req, res, next) => {
+    const { username, password } = req.body;
+    let statusCode, response;
+
+    if (!username || !password) {
+      res.status(400).end();
+      return;
+    }
+    if (!Object.keys(accountInfo).includes(username)) {
+      res.status(400).end();
+      return;
+    }
+    if (accountInfo[username]["password"] !== password) {
+      res.status(401).end();
+      return;
+    }
+    response = { status: "Successfully logined" };
+    req.session.name = username;
+    res.status(201).send({ username });
+  })
+);
+
+// Routing for react production and react routers
+router.route("/").get((req, res) => {
+  req.session.username = "guest";
+  res.sendFile(HTML_FILE); // EDIT
+});
+
+router.use(express.static(DIST_DIR)); // NEW
+
+router.get("/session", (req, res) => {
+  console.log(req.session.username);
+  if (req.session.username === "guest") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<p>You are a guest</p>");
+    res.end();
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<p>username: " + req.session.username + "</p>");
+    res.end();
+  }
+});
+
+function getCourseTime(serial_numbers) {
+  const CourseList = {};
+  const classInDay = {};
+  Constants.CLASS_IN_DAY.forEach((time) => {
+    classInDay[time] = [];
+  });
+  Constants.DAY_IN_WEEK.forEach((day) => {
+    CourseList[day] = JSON.parse(JSON.stringify(classInDay));
+  });
+  serial_numbers.forEach((serial_number) => {
+    const title = courseInfo[serial_number]["title"];
+    const time = courseInfo[serial_number]["time"];
+    time.forEach((day) => {
+      const specificDay = Object.keys(day)[0];
+      Object.values(day)[0].forEach((specificTime) => {
+        CourseList[specificDay][specificTime].push({
+          serial_number: serial_number,
+          title: title,
+        });
+      });
+    });
+  });
+  return CourseList;
+}
+
+module.exports = router;
---
+++ server/app.js
@@ -0,0 +1,102 @@
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const http = require("http");
+//const https = require("https");
+
+const express = require("express");
+//const logger = require("morgan");
+const session = require("express-session");
+const mongoose = require("mongoose");
+//const redis = require("redis");
+//const connectRedis = require("connect-redis");
+
+//const handleSocketEvents = require("./socket.js");
+const apiRouter = require("./api");
+
+// ========================================
+
+if (process.env.NODE_ENV === "development") {
+  console.log("NODE_ENV = development");
+}
+
+require("dotenv").config();
+
+const {
+  NODE_ENV,
+  HTTPS,
+  PORT,
+  //REDIS_HOST,
+  //SESSION_PREFIX,
+  MONGO_URL,
+  //MONGO_DB_NAME,
+} = process.env;
+const port = PORT || 3000;
+
+// ========================================
+
+const app = express();
+
+let server;
+let protocal = "http";
+
+if (NODE_ENV === "development" && protocal === "https") {
+  console.log("Use https in development");
+  protocal = "https";
+  const { SSL_CRT_FILE, SSL_KEY_FILE } = process.env;
+  const key = fs.readFileSync(SSL_KEY_FILE, "utf8");
+  const cert = fs.readFileSync(SSL_CRT_FILE, "utf8");
+  server = https.createServer({ key, cert }, app);
+} else {
+  server = http.createServer(app);
+  console.log("Using http protocol !!!");
+}
+
+// express app settings below
+
+const sessionOptions = {
+  cookie: {
+    path: "/",
+    httpOnly: true,
+    //secure: true,
+    maxAge: null,
+  },
+  resave: false,
+  saveUninitialized: false,
+  secret: uuidv4(),
+  unset: "destroy",
+  /*
+  store: new RedisStore({
+    client: redisClient,
+    prefix: SESSION_PREFIX,
+  }),
+  */
+};
+
+const sessionMiddleware = session(sessionOptions);
+app.use(sessionMiddleware);
+
+app.use("/", apiRouter);
+
+// db settings below
+
+const dbOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  auto_reconnect: true,
+  useUnifiedTopology: true,
+  poolSize: 10,
+};
+
+mongoose.connect(MONGO_URL, dbOptions);
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+
+db.once("open", () => {
+  console.log("Successfully connect to MongoDB!");
+
+  server.listen(port, () =>
+    console.log(`App listening at ${protocal}://localhost:${port}`)
+  );
+});
