ab7ac508efc10ca6fa994041719db4db60b5f6cf
Mason Chen <b08901058@ntu.edu.tw>
2020-12-29 09:28:22 +0800

    merge from frontend merge

--- src/App.js
+++ src/App.js
@@ -1,36 +1,30 @@
-import React, { useEffect, useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import {
-  HashRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { HashRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 import User from "./User/main";
-import clsx from "clsx";
-import Toolbar from "@material-ui/core/Toolbar";
-import Drawer from "@material-ui/core/Drawer";
-import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import MenuIcon from "@material-ui/icons/Menu";
-import Divider from "@material-ui/core/Divider";
-import IconButton from "@material-ui/core/IconButton";
+import clsx from 'clsx';
+import Toolbar from '@material-ui/core/Toolbar';
+import Drawer from '@material-ui/core/Drawer';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuIcon from '@material-ui/icons/Menu';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
 import Button from "@material-ui/core/Button";
-import Main from "./User/Mainpage/main";
-import Login from "./User/Login/main";
-import Reference from "./reference";
-import AppBar from "@material-ui/core/AppBar";
+import Main from "./User/Mainpage/main"
+import Login from "./User/Login/main"
+import Reference from "./reference"
+import AppBar from '@material-ui/core/AppBar';
 
 const useStyles = makeStyles((theme) => ({
   link: {
     textDecoration: "None",
   },
   hide: {
-    display: "none",
+    display: 'none',
   },
   drawer: {
     width: "100%",
@@ -55,55 +49,47 @@ export default () => {
   const handleDrawerClose = () => {
     setOpen(false);
   };
-  return (
-    <>
-      <AppBar position="fixed">
-        <Toolbar>
-          <div className={classes.title}>test</div>
-          <IconButton color="inherit" edge="end" onClick={handleDrawerOpen}>
-            <MenuIcon />
+  return (<>
+    <AppBar position="fixed">
+      <Toolbar>
+        <div className={classes.title}>test</div>
+        <IconButton color="inherit" edge="end" onClick={handleDrawerOpen}>
+          <MenuIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+    <Router>
+      <Drawer className={classes.drawer} anchor="top" open={open}>
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            <KeyboardArrowUpIcon />
           </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Router>
-        <Drawer className={classes.drawer} anchor="top" open={open}>
-          <div className={classes.drawerHeader}>
-            <IconButton onClick={handleDrawerClose}>
-              <KeyboardArrowUpIcon />
-            </IconButton>
-          </div>
-          <Divider />
-          <List>
-            {["User", "Login", "Reference"].map((text, index) => (
-              <ListItem button key={text}>
-                <Link
-                  to={"/" + text}
-                  className={classes.link}
-                  onClick={handleDrawerClose}
-                >
-                  {text}
-                </Link>
-              </ListItem>
-            ))}
-          </List>
-        </Drawer>
-        <div className={classes.top} />
-        <Switch>
-          <Route path="/User">
-            <User />
-          </Route>
-          <Route path="/Login">
-            <Login />
-          </Route>
-          <Route path="/Main/:serial_number">
-            <Main />
-          </Route>
-          <Route path="/Reference">
-            <Reference />
-          </Route>
-          <Redirect exact to="/Login" from="/" />
-        </Switch>
-      </Router>
-    </>
+        </div>
+        <Divider />
+        <List>
+          {['User', 'Login', 'Reference'].map((text, index) => (
+            <ListItem button key={text}>
+              <Link to={'/'+text} className={classes.link} onClick={handleDrawerClose}>{text}</Link>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <div className={classes.top} />
+      <Switch>
+        <Route path="/User">
+          <User />
+        </Route>
+        <Route path="/Login">
+          <Login />
+        </Route>
+        <Route path="/Main/:serial_number" >
+          <Main />
+        </Route>
+        <Route path="/Reference">
+          <Reference />
+        </Route>
+        <Redirect exact to="/Login" from='/' />
+      </Switch>
+    </Router></>
   );
-};
+}
