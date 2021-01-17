5afc6f6cacf1bc9bd986a405364f3b7694680e3a
Kenny <b08902052@ntu.edu.tw>
2021-01-17 21:27:46 +0800

    submit value func

--- src/User/Mainpage/submit.js
+++ src/User/Mainpage/submit.js
@@ -9,7 +9,6 @@ const useStyles = makeStyles((theme) => ({
     bottom: 0,
     paddingBottom: 20,
     width: "100%",
-    // marginBottom: 20,
   },
   formBox: {
     width: "80%",
@@ -21,11 +20,18 @@ const useStyles = makeStyles((theme) => ({
 
 export default () => {
   const classes = useStyles();
+  const handleEnter = (e) => {
+    if(e.keyCode === 13 && e.target.value !== '') {
+        console.log(e.target.value);
+        e.target.value = '';
+    }
+  }
   return (
     <BottomNavigation className={classes.bottom} >
-      <form className={classes.formBox} noValidate autoComplete="off" >
-          <TextField className={classes.text} id="standard-basic" label="Standard" />
-      </form>
+      <div className={classes.formBox}>
+      <TextField className={classes.text} id="standard-basic" 
+        label="Standard" onKeyDown={handleEnter} />
+      </div>
     </BottomNavigation>
   );
 }
