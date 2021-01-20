dea36c3148c91732d604c78a70db404e972e0a59
Mason Chen <b08901058@ntu.edu.tw>
2021-01-20 22:22:05 +0800

    rm couresInfo

--- server/app.js
+++ server/app.js
@@ -14,12 +14,9 @@ const connectRedis = require("connect-redis");
 
 const { handleSocketEvents } = require("./socket");
 const apiRouter = require("./routes");
-const Course = require("./models/course");
 
 const DIST_DIR = path.join(__dirname, "../dist"); // NEW
 const HTML_FILE = path.join(DIST_DIR, "index.html"); // NEW
-const courseInfo = require("../course_info/parsed_courses.json");
-const { deleteOne } = require("./models/following");
 
 // ========================================
 
